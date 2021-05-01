c2ea9ad6ffffb0d9f94f2525ae4b5528f65e1d22
minhcuong99er <senseicuong@gmail.com>
2021-05-01 23:19:34 +0700

    Update

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,44 +1,38 @@
 package com.example.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Context
-import android.view.View
-import android.view.inputmethod.InputMethod
-import android.view.inputmethod.InputMethodManager
-import android.view.inputmethod.InputMethodSession
 import androidx.databinding.DataBindingUtil
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val myName : MyName = MyName("Cuong")
+
+    private val myName: MyName = MyName("Minh Cuong")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*setContentView(R.layout.activity_main)*/
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
         binding.myName = myName
-
-        /*findViewById<Button>(R.id.done_button).setOnClickListener {
-            addNickname(it)
-        }*/
-        binding.button.setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
-    private fun addNickname (view: View) {
+
+    private fun addNickname(view: View) {
         binding.apply {
-            //nicknameText.text = nicknameEdit.text
             myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
-            view.visibility = View.GONE
+            doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
         }
-        // Hide the keyboard
-        var _imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        _imm.hideSoftInputFromWindow(view.windowToken, 0)
+        
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
